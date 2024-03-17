b806786c44a66d717816a8d5ede5ccd76c0b8179
Tazbiul Hasan <tazbiul.h31@gmail.com>
2024-03-17 13:24:13 +0600

    intro message

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,4 +1,6 @@
-X =  input("What is the name of your city?\n ")
-Y = input("What is the name of your pet?\n ")
+print("Welcome to the band name generator\n")
+
+X =  input("What city did you grow up in?\n ")
+Y = input("What is the name of your first pet?\n ")
 
 print("Your band name is: "+ X+" " + Y)
